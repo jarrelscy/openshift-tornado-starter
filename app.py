7aaad6de4ba272a1c55809193c3349f7600cf4c9
Jarrel Seah <jarrelscy@gmail.com>
2018-09-13 18:15:04 +1000

    Update app.py

--- app.py
+++ app.py
@@ -10,17 +10,10 @@
 import imp
 import os
 
-try:
-   zvirtenv = os.path.join(os.environ['OPENSHIFT_PYTHON_DIR'],
-                           'virtenv', 'bin', 'activate_this.py')
-   execfile(zvirtenv, dict(__file__ = zvirtenv) )
-except IOError:
-   pass
+
 
 if __name__ == '__main__':
-   ip   = os.environ['OPENSHIFT_PYTHON_IP']
-   port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
    app = imp.load_source('application', 'main.py')
 
-   app.application.listen(port , ip)
+   app.application.listen(8080 , '0.0.0.0')
    app.ioloop.IOLoop.instance().start()
