733c40de26999a05f7b8e7e0b8fab9e79d040e7a
Jarrel Seah <jarrelscy@gmail.com>
2018-09-13 18:06:24 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='Avinash Sajjanshetty',
+setup(name='jarrelscy',
       version='0.1',
       description='Tornado Starter Kit',
-      author='Avinash Sajjanshetty',
-      author_email='avinashsajjan@gmail.com',
-      url='http://www.python.org/sigs/distutils-sig/',
+      author='jarrelscy',
+      author_email='jarrelscy@gmail.com',
+      url='',
       install_requires=['tornado', 'requests', 'beautifulsoup4'],
      )
